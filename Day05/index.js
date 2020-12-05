0bfffd6528df227dd1d1db2f181a7a0cfaaae031
Nathan Richards <nld@logios.co.uk>
2020-12-05 17:24:15 +0000

    Refactored day 5

--- Day05/index.js
+++ Day05/index.js
@@ -1,24 +1,20 @@
 const fs = require('fs');
 
 const content = fs.readFileSync('input.txt', 'utf-8');
+
 const passes = content.trim().split('\n');
 
-function seatNum(pass) {
-    return parseInt(
-        pass.replace(/B|R/g,'1').replace(/F|L/g,'0'),
-        2);
-}
+// Convert each pass to a binary number
+const seatNums = passes
+    .map(p => p.replace(/B|R/g,'1'))
+    .map(p => p.replace(/F|L/g,'0'))
+    .map(p => parseInt(p, 2));
 
-const seatNums = passes.map(seatNum);
-
-// Part 1
-const maxNum = seatNums.reduce((p, c) => Math.max(p, c));
-console.log(maxNum);
-
-// Part 2
 seatNums.sort((a, b) => a - b);
 
-let i;
-for (i=0; seatNums[i] === seatNums[i+1] - 1; i++);
+// Part 1
+console.log(seatNums[seatNums.length-1]);
 
-console.log(seatNums[i]+1);
+// Part 2
+const previousSeat = seatNums.find((v, i) => v !== seatNums[i+1]-1); // finds the first non-sequential seat number
+console.log(previousSeat+1);
