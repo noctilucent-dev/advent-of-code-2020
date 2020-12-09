4efd3ded2a826d58da22eafabec955dd7274346e
Nathan Richards <nld@logios.co.uk>
2020-12-09 18:20:32 +0000

    Refactored day 9

--- Day09/index.js
+++ Day09/index.js
@@ -5,10 +5,13 @@ const content = fs.readFileSync('input.txt', 'utf-8');
 const PREAMBLE = 25;
 
 function part1(nums) {
+    // Iterate over each number after the preamble
     for(let i=PREAMBLE; i<nums.length; i++) {
         const target = nums[i];
         let found = false;
 
+        // Check each unique pair from the preceeding numbers
+        // Stopping if we find a matching sum
         for(let a=i-1; !found && a>i-PREAMBLE; a--) {
             for(let b=a-1; !found && b>=i-PREAMBLE; b--) {
                 found = nums[a] + nums[b] === target;
@@ -22,16 +25,19 @@ function part1(nums) {
 }
 
 function part2(nums, target) {
+    // Iterate over sub-sequences
     for(let start=0; start<nums.length-1; start++) {
-        if (nums[start] === target) continue;
+        let sum, end;
 
-        let sum = nums[start];
-        for(let end=start+1; sum < target && end<nums.length; end++) {
-            sum += nums[end];
-            if (sum === target) {
-                const range = nums.slice(start, end+1);
-                return Math.max(...range) + Math.min(...range);
-            }
+        // Add numbers until we match or exceed target
+        for(sum = nums[start], end=start+1;
+            sum<target && end<nums.length;
+            sum += nums[end++]);
+
+        // Check if there was an exact match
+        if (sum === target) {
+            const range = nums.slice(start, end+1);
+            return Math.max(...range) + Math.min(...range);
         }
     }
 }
