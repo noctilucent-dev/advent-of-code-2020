b6f8f3ba56dc07a11b13e2e6adfa954c5d0ca842
Nathan Richards <nld@logios.co.uk>
2020-12-11 12:39:19 +0000

    Refactored day 11

--- Day11/index.js
+++ Day11/index.js
@@ -1,162 +1,94 @@
 const fs = require('fs');
 
-let content = fs.readFileSync('input.txt', 'utf-8');
+const content = fs.readFileSync('input.txt', 'utf-8');
 
-let initial = content.trim().split('\n').map(l => l.split(''));
+const initial = content.trim().split('\n').map(l => l.split(''));
 
 const WIDTH = initial[0].length;
 const HEIGHT = initial.length;
 
-function print(grid) {
-    for(let i=0; i<grid.length; i++) {
-        console.log(grid[i].join(''));
-    }
+// Vectors for the 8 directions
+const VECTORS = [
+    [-1, -1], [0, -1], [1, -1],
+    [-1, 0], [1, 0],
+    [-1, 1], [0, 1], [1, 1],
+];
+
+// Determines whether the given co-ordinates are inside the grid
+function inBounds (x, y) {
+    return x >= 0 && y >=0 && x < WIDTH && y < HEIGHT;
 }
 
-function countAdjacentPart1(grid, x, y) {
-    let count = 0;
-
-    if (y > 0) {
-        if (x > 0 && grid[y-1][x-1] === '#') count++;
-        if (grid[y-1][x] === '#') count++;
-        if (x < (WIDTH-1) && grid[y-1][x+1] === '#') count++;
-    }
-
-    if (x > 0 && grid[y][x-1] === '#') count++;
-    if (x < (WIDTH-1) && grid[y][x+1] === '#') count++;
-
-    if (y<HEIGHT-1) {
-        if (x > 0 && grid[y+1][x-1] === '#') count++;
-        if (grid[y+1][x] === '#') count++;
-        if (x < (WIDTH-1) && grid[y+1][x+1] === '#') count++;
-    }
-
-    return count;
+// Counts the occupied seats in the immediately adjacent positions
+function countAdjacent(grid, x, y) {
+    return VECTORS
+        .map(([dx, dy]) => [x+dx, y+dy])
+        .filter(([x, y]) => inBounds(x, y))
+        .filter(([x, y]) => grid[y][x] === '#')
+        .length;
 }
 
-function nextStatePart1(grid) {
-    let newGrid = grid.map(l => [...l]);
-    let changes = 0;
-
-    for(let y=0; y<HEIGHT; y++) {
-        for(let x=0; x<WIDTH; x++) {
-            if (grid[y][x] === '.') continue;
-
-            const count = countAdjacentPart1(grid, x, y);
-
-            if (count === 0 && grid[y][x] === 'L') {
-                newGrid[y][x] = '#';
-                changes++;
-            } else if (count >= 4 && grid[y][x] === '#') {
-                newGrid[y][x] = 'L';
-                changes++;
-            }
+// Counts the visibile occupied seats from the given position
+function countVisible(grid, x, y) {
+    const search = ([dx, dy]) => {
+        for (let cx=x+dx, cy=y+dy; inBounds(cx, cy); cx += dx, cy += dy) {
+            if (grid[cy][cx] === '#') return 1;
+            if (grid[cy][cx] === 'L') return 0;
         }
+        return 0;
     }
 
-    return [newGrid, changes];
+    return VECTORS.map(search).reduce((a, b) => a+b);
 }
 
-function countVisiblePart2(grid, x, y) {
-    let count = 0;
-
-    // Left
-    for(let dx=x-1; dx>=0; dx--) {
-        if (grid[y][dx] === '#') count++;
-        if (grid[y][dx] !== '.') break;
-    }
-
-    // Right
-    for(let dx=x+1; dx<WIDTH; dx++) {
-        if (grid[y][dx] === '#') count++;
-        if (grid[y][dx] !== '.') break;
-    }
-
-    // Up
-    for(let dy=y-1; dy>=0; dy--) {
-        if (grid[dy][x] === '#') count++;
-        if (grid[dy][x] !== '.') break;
-    }
-
-    // Down
-    for(let dy=y+1; dy<HEIGHT; dy++) {
-        if (grid[dy][x] === '#') count++;
-        if (grid[dy][x] !== '.') break;
-    }
-
-    // Up-left
-    for(let dy=y-1, dx=x-1; dy>=0 && dx>=0; dy--, dx--) {
-        if (grid[dy][dx] === '#') count++;
-        if (grid[dy][dx] !== '.') break;
-    }
-
-    // Up-Right
-    for(let dy=y-1, dx=x+1; dy>=0 && dx<WIDTH; dy--, dx++) {
-        if (grid[dy][dx] === '#') count++;
-        if (grid[dy][dx] !== '.') break;
-    }
-
-    // Down-right
-    for(let dy=y+1, dx=x+1; dy<HEIGHT && dx<WIDTH; dy++, dx++) {
-        if (grid[dy][dx] === '#') count++;
-        if (grid[dy][dx] !== '.') break;
-    }
-
-    // Down-left
-    for(let dy=y+1, dx=x-1; dy<HEIGHT && dx>=0; dy++, dx--) {
-        if (grid[dy][dx] === '#') count++;
-        if (grid[dy][dx] !== '.') break;
-    }
-
-    return count;
-}
-
-function nextStatePart2(grid) {
-    let newGrid = grid.map(l => [...l]);
-    let changes = 0;
-
-    for(let y=0; y<HEIGHT; y++) {
-        for(let x=0; x<WIDTH; x++) {
-            if (grid[y][x] === '.') continue;
-
-            const count = countVisiblePart2(grid, x, y);
-
-            if (count === 0 && grid[y][x] === 'L') {
-                newGrid[y][x] = '#';
-                changes++;
-            } else if (count >= 5 && grid[y][x] === '#') {
-                newGrid[y][x] = 'L';
-                changes++;
-            }
-        }
-    }
-
-    return [newGrid, changes];
-}
-
+// Counts the total number of occupied seats in the grid
 function countOccupied(grid) {
-    let count = 0;
+    return grid.flat().filter(c => c === '#').length;
+}
+
+// Uses rules to determine the next grid state from the current grid state.
+// grid            - the current state
+// counter         - the function for counting occupied seats
+// vacateThreshold - the threshold for vacating a seat
+function nextState(grid, counter, vacateThreshold) {
+    let newGrid = grid.map(l => [...l]);
+    let changed = false;
+
     for(let y=0; y<HEIGHT; y++) {
         for(let x=0; x<WIDTH; x++) {
-            if (grid[y][x] === '#') count++;
+            if (grid[y][x] === '.') continue;
+
+            const count = counter(grid, x, y);
+            if (count === 0) newGrid[y][x] = '#';
+            else if (count >= vacateThreshold) newGrid[y][x] = 'L';
+
+            changed = changed || grid[y][x] !== newGrid[y][x];
         }
     }
-    return count;
+
+    if (changed) return newGrid;
+}
+
+// Runs the simulation until a stable state is found
+// Returns the stable state
+// grid            - the initial state
+// counter         - the function for counting occupied seats
+// vacateThreshold - the threshold for vacating a seat
+function findStableState(grid, counter, vacateThreshold) {
+    let previous = initial, next;
+    while (true) {
+        next = nextState(previous, counter, vacateThreshold);
+        if (!next) break;
+        previous = next;
+    }
+
+    return previous;
 }
 
 // Part 1
-let grid = initial;
-let changes;
-while (true) {
-    [grid, changes] = nextStatePart1(grid);
-    if (changes === 0) break;
-}
-console.log(countOccupied(grid));
+let stable = findStableState(initial, countAdjacent, 4);
+console.log(countOccupied(stable));
 
 // Part 2
-grid = initial;
-while (true) {
-    [grid, changes] = nextStatePart2(grid);
-    if (changes === 0) break;
-}
-console.log(countOccupied(grid));
+stable = findStableState(initial, countVisible, 5);
+console.log(countOccupied(stable));
